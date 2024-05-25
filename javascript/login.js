c9aba2d7dede3835131be5982ead69f30be8ac41
foxyglue <nperdana2004@gmail.com>
2024-05-25 23:39:39 +0700

    ubah nama Id Username dan Password

--- javascript/login.js
+++ javascript/login.js
@@ -3,8 +3,8 @@ const loginForm = document.getElementById('login-form');
 loginForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = document.getElementById('Username').value;
+    const password = document.getElementById('Password').value;
 
     if (username === '' || password === '') {
         alert('Username dan password tidak boleh kosong!');
