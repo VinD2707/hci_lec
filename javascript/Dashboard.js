bf974c7eacce1547a88b3e0a5e372c5bf947c230
DavidPaulOng <davidpaulong@gmail.com>
2024-05-25 16:36:37 +0700

    added billing bar chart

--- javascript/Dashboard.js
+++ javascript/Dashboard.js
@@ -30,9 +30,21 @@ let department_labels = []
 let department_instance = null
 const department_chart = document.getElementById('departmentratio'); // HTML Chart
 
-window.addEventListener('resize', function () { appointment(); department(); })
+let billings_data = []
+let billings_labels = []
+let billings_instance = null
+const billings_chart = document.getElementById('billings'); // HTML Chart
+
+let billings_bar_data = []
+let billings_bar_labels = []
+let billings_bar_instance = null
+const billings_bar_chart = document.getElementById('billingsdepartmentratio'); // HTML Chart
+
+window.addEventListener('resize', function () { appointment(); department(); billing(); billing_pie();})
 
 async function getData(){
+
+    // appointmetns
     let response = await fetch('../csv/Appointments.csv');
     let data = await response.text();
     let table = data.split('\n').slice(1);
@@ -42,7 +54,8 @@ async function getData(){
         appointment_data.push(temp);
     })
 
-    response = await fetch('../csv/Departments.csv');
+    // appointment per departments
+    response = await fetch('../csv/Appointment-Departments.csv');
     data = await response.text();
     table = data.split('\n');
 
@@ -53,8 +66,39 @@ async function getData(){
 
         department_data.push(temp);
     })
+
+    // billing
+    response = await fetch('../csv/Billings.csv');
+    data = await response.text();
+    table = data.split('\n');
+
+    billings_labels = table[0].split(';')
+
+    table.slice(1).forEach(element => {
+        const temp = element.split(';');
+
+        billings_data.push(temp);
+    })
+
+    // billing per department
+
+    response = await fetch('../csv/Billing-Departments.csv');
+    data = await response.text();
+    table = data.split('\n');
+
+    billings_bar_labels = table[0].split(';')
+
+    table.slice(1).forEach(element => {
+        const temp = element.split(';');
+
+        billings_bar_data.push(temp);
+    })
+
+    console.log(billings_bar_data)
     appointment();  
-    department()
+    department();
+    billing();
+    billing_pie();
     
 }
 
@@ -166,7 +210,7 @@ function department(){
 
         datasets: [{
             data: department_data[0],
-            borderWidth: 3,
+            borderWidth: 2,
             backgroundColor: ['#55efc4', "#81ecec", "#74b9ff"],
             borderColor: 'rgb(0,0,0)',
             tension: 0.1
@@ -226,6 +270,147 @@ function department(){
 
 
 
+    function billing(){
+        if(billings_instance != null){
+            billings_instance.destroy();
+        };
+        
+        
+        const startDate = new Date('2024-05-24');
+        const datesList = getdates(startDate, (billings_data[0].length));
+        
+        billings_instance = new Chart(billings_chart, {
+            maintainAspectRatio: false,
+            type: 'line',
+            data: {
+            labels: datesList ,
+            datasets: [{
+                label: 'Doctor Status',
+                data: billings_data[0],
+                backgroundColor: 'rgb(0,0,0)',
+                borderWidth: 2,
+                
+                borderColor: 'rgb(0,0,0)',
+                tension: 0.1
+            }]
+            },
+            
+            options: {
+                scales: {
+                    x: {
+                        ticks: {
+                            maxRotation: 30, 
+                            minRotation: 30,
+                            font: {
+                                size: 22
+                                
+                            }
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            font: {
+                                size: 22
+                                
+                            }
+                        }
+                    }
+                    
+                },
+        
+                      
+                
+                aspectRatio: 2.7,
+                responsive: true,
+                plugins: {
+                    
+        
+                legend: {
+                    display: false
+                },
+                title:{
+                    display:true,
+                    // text: "Doctors in Each Department",
+                    font:{
+                    size: 28
+                    }
+                },
+                
+            },
+           
+            }
+        });
+        }
+
+
+// billing pie
+
+function billing_pie(){
+    if(billings_bar_instance != null){
+        billings_bar_instance.destroy();
+    };
+    
+    billings_bar_instance = new Chart(billings_bar_chart, {
+        maintainAspectRatio: false,
+        type: 'bar',
+        data: {
+        labels: billings_bar_labels,
+        datasets: [{
+            label: 'Doctor Status',
+            data: billings_bar_data[0],
+            borderWidth: 2,
+            borderColor: 'rgb(0,0,0)',
+            backgroundColor: ['#55efc4', "#81ecec", "#74b9ff"]
+        }]
+        },
+        
+        options: {
+    
+            scales: {
+                x: {
+                    ticks: {
+                        font: {
+                            size: 22
+                            
+                        },
+                    }
+                },
+
+                y: {
+                    ticks: {
+                        font: {
+                            size: 20
+                            
+                        }
+                    }
+                }
+                
+            },
+                  
+            
+            aspectRatio: 1.3,
+            responsive: true,
+            plugins: {
+    
+            legend: {
+                display: false
+            },
+            title:{
+                display:true,
+                text: "     Billings From Each Department",
+                font:{
+                size: 22
+                }
+            },
+            
+        },
+       
+        }
+    });
+    }
+    
+
+
     // Table Status Update 
 
 
@@ -257,5 +442,26 @@ window.addEventListener('load', function() {
     
 
 })
+
+
+// update total billings
+
+
+window.addEventListener('load', function() {
+
+    const billingsbubble = document.getElementById('billingsbubble');
+    const total_billing = billingsbubble.getElementsByClassName('statvalue')[0];
+
+    let temp = 0
+    console.log(billings_bar_data[0][0])
+    for(i = 0; i < billings_bar_data[0].length; i++){
+            temp += parseInt(billings_bar_data[0][i])
+            console.log(temp)
+        }
+
+    total_billing.innerHTML = temp.toLocaleString()
     
+    
+
+})
 
