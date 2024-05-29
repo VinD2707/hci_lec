227a1b15f85bf5ece14d190392f397432aba4ee7
foxyglue <nperdana2004@gmail.com>
2024-05-29 10:17:53 +0700

    function search nama specialization

--- javascript/Spesialization.js
+++ javascript/Spesialization.js
@@ -14,3 +14,22 @@ document.querySelectorAll('.dropdown-head').forEach(function(head) {
         dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
     });
 });
+
+
+//searchh
+
+function Search() {
+    var cari = document.getElementById('search').value.toLowerCase();
+    var specialization = document.getElementsByClassName('row');
+
+    for (var i = 0; i < specialization.length; i++) {
+        var doctor_name = specialization[i].getElementsByClassName('name-sp')[0].textContent.toLowerCase();
+        console.log(cari);
+
+        if (doctor_name.includes(cari)) {
+            specialization[i].style.display = 'table-row';
+        } else {
+            specialization[i].style.display = 'none';
+        }
+    }
+}
